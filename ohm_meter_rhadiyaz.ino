ae54b1d5f1116808a160de7e52c8068ed032cdce
rhadiyazgamaputra32 <radiasgama@gmail.com>
2016-12-05 21:56:50 +0700

    ohm_meter
    ohm_meter

---
+++ ohm_meter_rhadiyaz.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ pinMode(A0,INPUT);
+ digitalWrite(A0,0);
+
+ int Nilaisensor= analogRead(A0);
+ 
+
+  float Tegangan = Nilaisensor * ( 5.0/1023.0);
+  Serial.print("Tegangan = ");Serial.println(Tegangan); 
+  
+float i = (5.0 - Tegangan )/ 100.0 ;
+
+ float R2 = Tegangan / i;
+ Serial.print("resistor 2 = "); Serial.println(R2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
